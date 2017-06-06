c8954a15e1ab1bc4def00a90bfccd09bda237bb6
Albert So <aso@atlassian.com>
2017-06-05 17:47:27 -0700

    commented out all the brew stuff

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-brew update
-brew install rbenv
-rbenv init
-rbenv install 2.0.0-p247
+#brew update
+#brew install rbenv
+#rbenv init
+#rbenv install 2.0.0-p247
 
-gem install cocoapods --pre --no-rdoc --no-ri --no-document --quiet
+#gem install cocoapods --pre --no-rdoc --no-ri --no-document --quiet
 
 xcodebuild -version
 xcodebuild -showsdks
