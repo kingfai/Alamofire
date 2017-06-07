3ba30ab0749ab43df5219da0bbba578ca1b235b6
Albert So <aso@atlassian.com>
2017-06-07 11:54:09 -0700

    seeing if sudo gets gem install to work on buddybuild

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -5,7 +5,7 @@
 #rbenv init
 #rbenv install 2.0.0-p247
 
-#gem install cocoapods --pre --no-rdoc --no-ri --no-document --quiet
+sudo gem install cocoapods --pre --no-rdoc --no-ri --no-document --quiet
 
 xcodebuild -version
 xcodebuild -showsdks
